241ae84698353cb8b80af0482a6e07f5b6425181
Francisco Dueñas <Sebasduca@gmail.com>
2025-12-11 10:34:28 -0500

    feat(ms-send-email): Enhance EmailRequest model with OpenAPI annotations for improved documentation

--- ms-send-email/src/main/java/co/com/training/model/request/EmailRequest.java
+++ ms-send-email/src/main/java/co/com/training/model/request/EmailRequest.java
@@ -2,11 +2,38 @@ package co.com.training.model.request;
 
 import java.util.List;
 
-public record EmailRequest(
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+public record EmailRequest( 
+    /*
+     * Email identifier
+     * @param emailId The unique identifier of the email
+     */
+    @Schema(description = "Email identifier", example = "1", required = true)
     String emailId,
-    String emailTo,
-    String emailSubject,
-    String emailBody,
+    /*
+     * Email recipient address
+     * @param recipient The recipient email address
+     */
+    @Schema(description = "Email recipient address", example = "developer@training.com", required = true)
+    String recipient,
+    /*
+     * Email subject
+     * @param subject The subject of the email
+     */
+    @Schema(description = "Email subject", example = "Test email", required = true)
+    String subject,
+    /*
+     * Email message
+     * @param message The message of the email
+     */
+    @Schema(description = "Email message", example = "This is a test email", required = true)
+    String message,
+    /*
+     * Email attacheds
+     * @param emailAttacheds The attacheds of the email
+     */
+    @Schema(description = "Email attacheds", example = "This is a test email", required = true)
     List<EmailAttached> emailAttacheds
 ) {
 }
