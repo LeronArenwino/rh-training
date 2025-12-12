4b3ebc15f62873e7a10ae45f4f9c8a57774eefa6
Francisco Dueñas <Sebasduca@gmail.com>
2025-12-12 17:00:02 -0500

    feat(ms-send-email): Enhance resendEmail endpoint with request validation and logging

--- ms-send-email/src/main/java/co/com/training/resource/EmailResource.java
+++ ms-send-email/src/main/java/co/com/training/resource/EmailResource.java
@@ -1,16 +1,26 @@
 package co.com.training.resource;
 
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.jboss.logging.Logger;
+
 import co.com.training.model.request.EmailRequest;
 import co.com.training.model.response.DataResponse;
 import co.com.training.service.EmailService;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/v1")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class EmailResource {
     
+    private static final Logger LOG = Logger.getLogger(EmailResource.class);
     private final EmailService emailService;
 
     @Inject
@@ -20,7 +30,8 @@ public class EmailResource {
 
     @POST
     @Path("/resend")
-    public Uni<DataResponse> resendEmail(EmailRequest emailRequest) {
+    public Uni<DataResponse> resendEmail(@RequestBody @Valid EmailRequest emailRequest) {
+        LOG.info("Resending email to: " + emailRequest.recipient());
         return emailService.sendEmail(emailRequest);
     }
 }
