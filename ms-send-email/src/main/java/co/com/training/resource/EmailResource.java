affc5ffcb0e8a3c69b475006042e67a5685a136b
Francisco Dueñas <Sebasduca@gmail.com>
2025-12-12 16:29:33 -0500

    feat(ms-send-email): Implement resendEmail endpoint in EmailResource for email resending functionality

--- ms-send-email/src/main/java/co/com/training/resource/EmailResource.java
+++ ms-send-email/src/main/java/co/com/training/resource/EmailResource.java
@@ -1,10 +1,26 @@
 package co.com.training.resource;
 
+import co.com.training.model.request.EmailRequest;
+import co.com.training.model.response.DataResponse;
+import co.com.training.service.EmailService;
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
-@Path("/api/v1/admin/resend")
+@Path("/api/v1")
 public class EmailResource {
     
+    private final EmailService emailService;
 
-    
+    @Inject
+    public EmailResource(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @POST
+    @Path("/resend")
+    public Uni<DataResponse> resendEmail(EmailRequest emailRequest) {
+        return emailService.sendEmail(emailRequest);
+    }
 }
