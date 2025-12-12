7e5006285f4666040b82a173739929fa9b5ad4e3
Francisco Dueñas <Sebasduca@gmail.com>
2025-12-12 17:00:11 -0500

    feat(ms-send-email): Implement sendEmail method in EmailServiceImpl to return a success response

--- ms-send-email/src/main/java/co/com/training/service/EmailServiceImpl.java
+++ ms-send-email/src/main/java/co/com/training/service/EmailServiceImpl.java
@@ -2,6 +2,8 @@ package co.com.training.service;
 
 import co.com.training.model.request.EmailRequest;
 import co.com.training.model.response.DataResponse;
+import co.com.training.model.response.Header;
+import co.com.training.model.response.Body;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -10,7 +12,16 @@ public class EmailServiceImpl implements EmailService{
 
     @Override
     public Uni<DataResponse> sendEmail(EmailRequest emailRequest) {
-        return Uni.createFrom().item(null);
+        // Temporary response
+        Header header = new Header(200, "Sent");
+        Body body = new Body(
+            emailRequest.emailId(),
+            emailRequest.recipient(),
+            "SUCCESS",
+            "Email sent successfully to the SOAP service"
+        );
+        DataResponse response = new DataResponse(header, body);
+        return Uni.createFrom().item(response);
     }
 
 }
